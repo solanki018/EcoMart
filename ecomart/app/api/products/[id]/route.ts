958d49b7d5c12dd0ad0f4b15e5a0ed1d69a2cea5
Sourabh_ <sourabhsolanki1694@gmail.com>
2025-10-23 13:05:20 +0530

    deployment bug fix

--- ecomart/app/api/products/[id]/route.ts
+++ ecomart/app/api/products/[id]/route.ts
@@ -1,15 +1,16 @@
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import connectDB from "../../../lib/dbConnect";
 import Product from "../../../models/Product";
-import { NextRequest } from "next/server";
 
-export async function PATCH(req: NextRequest, context: { params: { id: string } }) {
+export async function PATCH(
+  req: NextRequest,
+  context: { params: Promise<{ id: string }> } // <- note the Promise here
+) {
+  const { params } = context;
+  const { id } = await params; // await the params promise
   await connectDB();
-  const { id } = context.params;
   const body = await req.json();
 
   const updated = await Product.findByIdAndUpdate(id, body, { new: true });
-  if (!updated) return NextResponse.json({ error: "Product not found" }, { status: 404 });
-
   return NextResponse.json(updated);
 }
